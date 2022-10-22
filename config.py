4b94e2c7bd04546f80f96f7895603dcab02cbac9
Adeyronxx <aderonke.adegbite@yahoo.com>
2022-10-22 16:24:16 -0500

    third commit

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
 # ADD API Key
 api_key = "tiQDgzI8IIeXqI5t82cEalzSVn41ZW1H"
-weather_api_key= "a682da094de596c3a11cf7ee722f1a7a"
+weather_api_key = "a682da094de596c3a11cf7ee722f1a7a"
+g_key = 'AIzaSyAbwFGmtG8XfjFmji2uvgAjMdyGPjcJ78w'
